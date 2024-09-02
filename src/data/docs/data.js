642010605ed13c29065d46ce56de879ceef56a5f
yaxyobekb <pubgn9642@gmail.com>
2024-09-02 20:48:29 +0500

    add `docs` initial datas

---
+++ src/data/docs/data.js
@@ -0,0 +1,11 @@
+import docImage1 from "../../assets/images/docs/docs-image-1.png";
+
+export const docsData = [
+  {
+    id: 0,
+    title: "Botga kirish",
+    image: docImage1,
+    description:
+      "Telegramda [ClaimBlumTokens](https://t.me/claimblumtokensbot) botini oching",
+  },
+];
